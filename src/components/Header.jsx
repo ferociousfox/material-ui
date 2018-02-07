cefe8dc020191ee2b485713271031c7f14c82604
Ginger Lee Kretschmer, Luke Bertram and Shane Ryan <ginger+luke+shane@>
2018-02-07 11:53:50 -0800

    add style reset to root component, add images to project

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import hamburger from '../assets/images/hamboiga.svg';
 
 function Header(){
   return (
-    <div>
-      <img src="hamburger"/>
+    <div className="nav">
+      <img src={hamburger}/>
+      <style jsx>{`
+        .nav {
+          background-color: #00bcd4;
+          height: 64px;
+        }
+        img {
+          width: 50px;
+          height: auto;
+        }
+      `}</style>
     </div>
   );
 }
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,10 +4,16 @@ import Header from './../components/Header';
 function App() {
   return (
     <div>
-
+      <style jsx global>{`
+        body{
+          margin: 0;
+          padding: 0;
+          box-styling: border-box;
+        }
+      `}</style>
       <Header/>
 
-      <div class="hero">
+      <div className="hero">
         <img src="logo"/>
         <div>
           <h1>Material-UI</h1>
@@ -16,23 +22,23 @@ function App() {
         </div>
       </div>
 
-      <div class="description">
-        <p>Material-UI came about from our love of <link>React</link> and <link>Google's Material Design</link>. We are currently using it on a project at <link>Call-Em-All</link> and plan on adding to it and making it better in the coming months.</p>
+      <div className="description">
+        <p>Material-UI came about from our love of <a>React</a> and <a>Google's Material Design</a>. We are currently using it on a project at <a>Call-Em-All</a> and plan on adding to it and making it better in the coming months.</p>
       </div>
 
-      <div class="gallery">
-        <div class="card"></div>
-        <div class="card"></div>
-        <div class="card"></div>
+      <div className="gallery">
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
       </div>
 
-      <div class="promo">
+      <div className="promo">
         <p>Want to help make this project awesome? Check out our repo.</p>
       </div>
 
       <footer>
-        <p>Hand crafted with loathing by the engineers at <link>Call-Em-All</link></p>
-        <p>and our awesome <link>contributors</link>.</p>
+        <p>Hand crafted with loathing by the engineers at <a>Call-Em-All</a></p>
+        <p>and our awesome <a>contributors</a>.</p>
         <img src="githublogo.svg"/>
       </footer>
 
