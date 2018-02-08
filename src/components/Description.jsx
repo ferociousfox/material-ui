65f98d5044fec80ff58c905d386bad06d7cb236c
Ginger Lee Kretschmer, Luke Bertram and Shane Ryan <luke@>
2018-02-07 16:08:29 -0800

    add description styling

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -4,6 +4,20 @@ function Description(){
   return(
     <div className="description">
       <p>Material-UI came about from our love of <a>React</a> and <a>Google's Material Design</a>. We are currently using it on a project at <a>Call-Em-All</a> and plan on adding to it and making it better in the coming months.</p>
+      <style jsx>{`
+          .description {
+            height: 300px;
+            background-color: #eeeeee;
+            display: flex;
+            align-items: center;
+            padding: 30px;
+            font-size: 1.2em;
+            text-align: center;
+          }
+          a {
+            color: #fb6f9f;
+          }
+      `}</style>
     </div>
   );
 }
